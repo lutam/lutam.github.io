56f5d956f830e23a01af59f8c002038219540096
lutam <l.tamigio@gmail.com>
2020-01-31 22:49:46 +0100

    add virtual-dogs

---
+++ projects/virtual-dogs/precache-manifest.197c31b622340d20215e221cf86f553c.js
@@ -0,0 +1,110 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e347db224270df504fa5b2dc9ff91b24",
+    "url": "/projects/virtual-dogs/index.html"
+  },
+  {
+    "revision": "b84770b94b005fe8755b",
+    "url": "/projects/virtual-dogs/static/css/2.3fafe98e.chunk.css"
+  },
+  {
+    "revision": "0c7f6324026f604fab29",
+    "url": "/projects/virtual-dogs/static/css/main.96b8b453.chunk.css"
+  },
+  {
+    "revision": "b84770b94b005fe8755b",
+    "url": "/projects/virtual-dogs/static/js/2.cf4ed1a7.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/projects/virtual-dogs/static/js/2.cf4ed1a7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0c7f6324026f604fab29",
+    "url": "/projects/virtual-dogs/static/js/main.027c1def.chunk.js"
+  },
+  {
+    "revision": "cc6f69c252101ab8d455",
+    "url": "/projects/virtual-dogs/static/js/runtime-main.f643357d.js"
+  },
+  {
+    "revision": "e2de96bf2051a3832f932d252f8ecb2c",
+    "url": "/projects/virtual-dogs/static/media/batman.e2de96bf.png"
+  },
+  {
+    "revision": "43315acac37dd7c8b1245fc029dac301",
+    "url": "/projects/virtual-dogs/static/media/dosis-latin-200.43315aca.woff2"
+  },
+  {
+    "revision": "efb4f1b394b03f756ef78c315764df5b",
+    "url": "/projects/virtual-dogs/static/media/dosis-latin-200.efb4f1b3.woff"
+  },
+  {
+    "revision": "2b94af6b89900a290aed0e6db6d1d3ab",
+    "url": "/projects/virtual-dogs/static/media/dosis-latin-300.2b94af6b.woff2"
+  },
+  {
+    "revision": "962e1de7707064851359738cf50bcd3b",
+    "url": "/projects/virtual-dogs/static/media/dosis-latin-300.962e1de7.woff"
+  },
+  {
+    "revision": "94308ea01e3f1c8521f501eec0e31f4b",
+    "url": "/projects/virtual-dogs/static/media/dosis-latin-400.94308ea0.woff"
+  },
+  {
+    "revision": "9541c3cf46f91126f47595541a26e046",
+    "url": "/projects/virtual-dogs/static/media/dosis-latin-400.9541c3cf.woff2"
+  },
+  {
+    "revision": "25fbadc249579023699a5eb44f25fe64",
+    "url": "/projects/virtual-dogs/static/media/dosis-latin-500.25fbadc2.woff"
+  },
+  {
+    "revision": "f0ff7c323bfb7d3b07c20f2cb51c46e3",
+    "url": "/projects/virtual-dogs/static/media/dosis-latin-500.f0ff7c32.woff2"
+  },
+  {
+    "revision": "89604e123c6ae8d62acc82f8ac32dedf",
+    "url": "/projects/virtual-dogs/static/media/dosis-latin-600.89604e12.woff"
+  },
+  {
+    "revision": "e6945cb8ae05696c780871304a08ef47",
+    "url": "/projects/virtual-dogs/static/media/dosis-latin-600.e6945cb8.woff2"
+  },
+  {
+    "revision": "a5d68b5a0bd45d15addd718a8894c85e",
+    "url": "/projects/virtual-dogs/static/media/dosis-latin-700.a5d68b5a.woff"
+  },
+  {
+    "revision": "fff3aa8b593d77e9336f4a22397eed8f",
+    "url": "/projects/virtual-dogs/static/media/dosis-latin-700.fff3aa8b.woff2"
+  },
+  {
+    "revision": "28ded958d43c575ea7c992a324cb72d5",
+    "url": "/projects/virtual-dogs/static/media/dosis-latin-800.28ded958.woff2"
+  },
+  {
+    "revision": "8ba2fbedc0ebf5e34c9063a1048e0f60",
+    "url": "/projects/virtual-dogs/static/media/dosis-latin-800.8ba2fbed.woff"
+  },
+  {
+    "revision": "c23574b264e5c640dc14a33187abaa18",
+    "url": "/projects/virtual-dogs/static/media/grass.c23574b2.png"
+  },
+  {
+    "revision": "c79c5b1a0b4ce06710f4da3fab289af7",
+    "url": "/projects/virtual-dogs/static/media/pimpa.c79c5b1a.png"
+  },
+  {
+    "revision": "b942ee7c8e794e6cf98d32c28de69cbc",
+    "url": "/projects/virtual-dogs/static/media/pistorius.b942ee7c.png"
+  },
+  {
+    "revision": "bdc03cbc1b96655273d55c3a116e34c8",
+    "url": "/projects/virtual-dogs/static/media/sky.bdc03cbc.png"
+  },
+  {
+    "revision": "7ea21dac7278dc1eab6d5363730c8769",
+    "url": "/projects/virtual-dogs/static/media/unicorn.7ea21dac.png"
+  }
+]);
