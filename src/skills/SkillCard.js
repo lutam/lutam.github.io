1df7ced2cd2760a523740717c62ea7c8d23c9774
lutam <l.tamigio@gmail.com>
2025-11-01 19:17:18 +0000

    feat: skill page

---
+++ src/skills/SkillCard.js
@@ -0,0 +1,13 @@
+import "./SkillCard.css";
+
+export default function SkillCard({ skill, colorClass }) {
+
+  return (
+    <div className={`skill-card square-${colorClass}`}>
+        <div className="skill-card-inner">
+          <p>{skill.name}</p>
+          <p className="balls">{Array(skill.level).fill('●').join('')} {Array(3 - skill.level).fill('○').join('')}</p>
+        </div>
+    </div>
+  );
+}
--- src/skills/Skills.js
+++ src/skills/Skills.js
@@ -1,24 +1,53 @@
 import Screen from "../Screen";
 import "./../App.css";
+import SkillCard from "./SkillCard";
 
-export default function About() {
+const CORE = [
+  { name: "HTML", level: 3, slug: "html" },
+  { name: "CSS", level: 3, slug: "css" },
+
+  { name: "JavaScript", level: 3, slug: "javascript" },
+  { name: "TypeScript", level: 3, slug: "typescript" },
+  { name: "Python", level: 2, slug: "python" },
+  { name: "C++", level: 1, slug: "cpp" },
+];
+
+const color = ["one", "two", "three", "four", "five", "seven", "eight"];
+
+export default function Skills() {
   return (
     <Screen>
-      <div className="row">
+      <div className="row mt-large">
         <div className="col col-1">
-          <h1>SKILLS</h1>
+          <h1>Skills</h1>
         </div>
       </div>
 
       <div className="row">
         <div className="col col-1">
-          <p className="justify">
-            I have a strong background in computer science and web development,
-            with a passion for creating innovative solutions that push the
-            boundaries of what's possible on the web. My work spans various
-            domains, including interactive art, data visualization, and machine
-            learning applications.
+          <p className="justify mt-large">
+            My current set of tools… for now. The list changes as fast as
+            JavaScript frameworks.
           </p>
+        </div>
+      </div>
+
+      <div className="row mt-large">
+        <div className="col col-1">
+          ●●● Expert &nbsp; ●●○ Proficient &nbsp; ●○○ Familiar
+        </div>
+      </div>
+
+      <div className="row mt-large">
+        <div className="col col-1">
+          <h2>CORE</h2>
+        </div>
+      </div>
+      <div className="row ">
+        <div className="colcol-1 mt-large skill-grid">
+          {CORE.map((skill, i) => (
+            <SkillCard skill={skill} colorClass={color[i % color.length]} />
+          ))}
         </div>
       </div>
     </Screen>
